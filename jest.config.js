f973fb2dd62e519cbeab95b9479f083f270be06f
Claudio Junior <claudio.junior@luizalabs.com>
2024-11-24 17:39:13 -0300

    feat: configure TypeScript and Jest for improved testing and module resolution

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,38 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  collectCoverage: true,
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      }
+    ]
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+  },
+  coverageReporters: ['html', 'text', 'lcov', 'text-summary', 'json-summary'],
+  reporters: ['default'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
 };
